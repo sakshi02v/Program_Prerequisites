00e39898c12091a2a8e09d789a0f7bd79fad2a65
Sakshi Verma <sakshi02v@gmail.com>
2025-01-08 11:10:49 +0530

    VolumeOfCylinder Program

---
+++ program5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class program5{
+public static void main(String [] args){
+double pi=3.14;
+Scanner sc=new Scanner(System.in);
+int r=sc.nextInt();
+int h=sc.nextInt();
+double volume=pi*r*r*h;
+System.out.println("volume of cylinder:"+volume);
+}
+}
