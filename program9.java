e3226c5896487d6773126887c4286edcd349c7bc
Sakshi Verma <sakshi02v@gmail.com>
2025-01-08 11:17:16 +0530

    AverageOfThreeNumbers Program

---
+++ program9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class program9{
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter first number:");
+int a=sc.nextInt();
+System.out.println("Enter second number:");
+int b=sc.nextInt();
+System.out.println("Enter third number:");
+int c=sc.nextInt();
+double avg=(a+b+c)/3;
+System.out.println("Average is:"+avg);
+}
+}
