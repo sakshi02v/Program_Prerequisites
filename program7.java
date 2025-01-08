2833b75ae0a5195b967a1d967a1441f8d3657fc7
Sakshi Verma <sakshi02v@gmail.com>
2025-01-08 11:15:38 +0530

    PerimeterOfRectangle Program

---
+++ program7.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class program7{
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter length of rectangle:");
+int l=sc.nextInt();
+System.out.println("Enter width of rectangle:");
+int w=sc.nextInt();
+int per=2*(l+w);
+System.out.println("Perimeter of rectangle:"+per);
+}
+}
