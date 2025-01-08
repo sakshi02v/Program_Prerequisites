b09193ae30d2e13175da49114615993a31f0e046
Sakshi Verma <sakshi02v@gmail.com>
2025-01-08 11:17:46 +0530

    KilometersToMiles Program

---
+++ program10.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class program10{
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter kilometeres:");
+int k=sc.nextInt();
+double m=k* 0.621371;
+System.out.println("Miles:"+m);
+}
+}
