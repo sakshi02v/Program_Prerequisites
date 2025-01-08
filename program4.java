2bd865f127ffc848a4e364fa79bcd2d3ad7a28fc
Sakshi Verma <sakshi02v@gmail.com>
2025-01-08 11:09:56 +0530

    AreaOfCircle Program

---
+++ program4.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class program4{
+public static void main(String [] args){
+double pi=3.14;
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+double area=pi*a*a;
+System.out.println("Area of circle :"+area);
+}
+}
